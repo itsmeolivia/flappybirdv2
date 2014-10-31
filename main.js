4636cb50fd910480cb76f328963f0e06d86b8d7c
Olivia Nordquist <nolivia@umich.edu>
2014-10-31 10:31:53 -0400

    Begin to draw each frame

--- main.js
+++ main.js
@@ -8,5 +8,31 @@ document.body.appendChild(canvas);
 
 var context = canvas.getContext("2d");
 
-context.fillStyle = "#00ffff";
-context.fillRect(10, 10, 20, 20);
+var bird = {
+	x: canvas.width/2,
+	y: canvas.height/2
+};
+
+function physics() {
+	bird.y = 100 + 20 * Math.cos(new Date/100);
+	if(bird.x >= 0)
+		bird.x += 100;
+}
+
+function tik(t) {
+	draw();
+	physics();
+	requestAnimationFrame(tik);
+	console.log(t);
+}
+
+requestAnimationFrame(tik);
+
+//xywh
+function draw(){
+ context.clearRect(0, 0, canvas.width, canvas.height);
+ context.fillStyle = "#00ffff";
+ context.fillRect(bird.x - 10, bird.y - 10, 20, 20);
+}
+
+draw();
