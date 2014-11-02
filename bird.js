f6ec286458e2489b7a727fdf2791a95ef467f9f2
Olivia Nordquist <nolivia@umich.edu>
2014-11-02 15:17:44 -0500

    bird boundaries

--- bird.js
+++ bird.js
@@ -1,6 +1,6 @@
 function Bird(x, y){
 	this.x = x;
-	this.y = y;	
+	this.y = y;
 	this.velocity_x = 75;
 	this.velocity_y = 0;
 
@@ -11,17 +11,20 @@ function Bird(x, y){
 
 	});
 
-} 
+}
 Bird.prototype.update = function(dt) {
 
 	this.x += this.velocity_x * dt;
+	if (this.x < 20 || this.x > 495)
+		this.velocity_x = -this.velocity_x;
 	this.y += this.velocity_y * dt;
+	if (this.y < 20 || this.y > 665)
+		this.velocity_y = 0;
 
 	this.velocity_y += 100* dt; //gravitas
 };
 Bird.prototype.draw = function(context) {
 	context.fillStyle = "#99cc00";
-	context.fillRect(this.x - 10, this.y - 10, 20, 20);
+	context.fillRect(this.x - 20, this.y - 20, 40, 40);
 
 };
-
