42c8e793bf74b93997c49194f60a4326588fb661
Olivia Nordquist <nolivia@umich.edu>
2014-11-04 19:01:51 -0500

    movement refined

--- bird.js
+++ bird.js
@@ -1,13 +1,13 @@
 function Bird(x, y){
 	this.x = x;
 	this.y = y;
-	this.velocity_x = 75;
-	this.velocity_y = 0;
+	this.velocity_x = 225;
+	this.velocity_y = -150;
 
 	var me = this;
 
 	window.addEventListener('click', function() {
-		me.velocity_y -= 150;
+		me.velocity_y = -250;
 
 	});
 
@@ -21,7 +21,7 @@ Bird.prototype.update = function(dt) {
 	if (this.y < 20 || this.y > 665)
 		this.velocity_y = 0;
 
-	this.velocity_y += 100* dt; //gravitas
+	this.velocity_y += 400 * dt; //gravitas
 };
 Bird.prototype.draw = function(context) {
 	context.fillStyle = "#99cc00";
